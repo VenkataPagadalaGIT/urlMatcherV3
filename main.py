f0e86b2dedfc43cfb06d73177a452d76253b212a
Sarak Dahal <ssagamersdignity@gmail.com>
2023-02-12 23:21:44 +0545

    v4

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import streamlit_authenticator as stauth
 from PIL import Image
 from streamlit_option_menu import option_menu
 
-# st.beta_set_page_config(nav_menu_color='#150958')
+st.beta_set_page_config(nav_menu_color='#150958')
 
 
 # USER AUTH
@@ -29,11 +29,10 @@ if authentication_status == False:
 if authentication_status == None:
     st.warning("Please enter your username and password.")
 if authentication_status:
-
     with st.sidebar:
         selected = option_menu(
             menu_title="IMPRESSIVE",
-            options=["URL Matcher", "Content Estimator", "Content Estimator 2", "Logout"],
+            options=["URL Matcher", "URL Migration Tool", "Content Estimator", "Content Estimator 2", "Logout"],
             icons=["award", "calculator", "person-fill", "box-arrow-left"],
             menu_icon="house-door",
             default_index=0
@@ -42,8 +41,8 @@ if authentication_status:
         image = Image.open('impressive.jpg')
         st.image(image)
 
-        st.subheader(f"Welcome {name}")
-        st.subheader('Python URL / Redirect Mapping Tool with Default Value')
+        st.subheader(f"Welcome User")
+        st.subheader('Subheading here')
         st.subheader('Directions:')
         st.write(
             '- Upload complete crawl \n - Upload a list of 404s in.CSV format (URL column named URL) \n - Would not '
@@ -86,13 +85,13 @@ if authentication_status:
                 mainMeta = val['Meta Description'][0]
                 mainH1 = val['H1'][0]
                 df3 = pd.merge(df, df1, on='To')
-                df3 = df3[['Similarity', 'From', 'To', 'Title', 'Meta Description', 'H1']]
-                var = .45
-                df3.loc[df3["Similarity"] < var, "To"] = ROOTDOMAIN
+                df3 = df3[['Similarity']]
+                var = .40
+                df3.loc[df3["Similarity"] < var, "New URL"] = ROOTDOMAIN
                 df3.loc[df3["Similarity"] < var, "Title"] = mainTitle
                 df3.loc[df3["Similarity"] < var, "Meta Description"] = mainMeta
                 df3.loc[df3["Similarity"] < var, "H1"] = mainH1
-                # df3 = df3.sort_values(by='Similarity', ascending=False)
+                df3 = df3.sort_values(by='Similarity', ascending=False)
                 df3
 
 
@@ -105,6 +104,80 @@ if authentication_status:
             csv = convert_df(df3)
             st.download_button("Download Output", csv, "file.csv", "text/csv", key='download-csv')
 
+    if selected == "URL Migration Tool":
+        image = Image.open('impressive.jpg')
+        st.image(image)
+
+        st.subheader(f"Welcome User")
+        st.subheader('URL Migration Tool')
+        st.subheader('Directions:')
+        st.write(
+            '- Upload old crawl \n - Upload a list of new staging URL CSV format (URL column named URL) \n - Would not '
+            'recommend with over 10k URLs ')
+
+        st.write("Author - [Venkata Pagadala](https://www.linkedin.com/in/venkata-pagadala/)")
+        # Importing the URL CSV files
+        url0 = st.text_input('OLD URL', placeholder='Enter domain (www.google.com)')
+        url = st.text_input('NEW URL', placeholder='Enter domain (www.google.com)')
+
+        file1 = st.file_uploader("Upload Old Crawl")
+        file2 = st.file_uploader("Upload New Staging URL")
+        if file1 is not None and file2 is not None:
+            broken = pd.read_csv(file1)
+            current = pd.read_csv(file2)
+            ROOTDOMAIN0 = url0
+            ROOTDOMAIN = url
+            # Converting DF to List
+            broken_list = broken["Address"].tolist()
+            broken_list = [sub.replace(ROOTDOMAIN, '') for sub in broken_list]
+            current_list = current["Address"].tolist()
+            current_list = [sub.replace(ROOTDOMAIN, '') for sub in current_list]
+
+            for i in range(2, 3):
+                tfidf = TFIDF(n_gram_range=(i, i))
+                model = PolyFuzz(tfidf)
+                model.match(broken_list, current_list)
+                df1 = model.get_matches()
+                # Polishing and Pruning
+                df1["Similarity"] = df1["Similarity"].round(3)
+                index_names = df1.loc[df1['Similarity'] < .10].index
+                amt_dropped = len(index_names)
+                df1.drop(index_names, inplace=True)
+                df1["To"] = ROOTDOMAIN + df1["To"]
+                df1["From"] = df1["From"]
+
+                df = pd.DataFrame()
+                df['To'] = current['Address']
+                df['Title'] = current['Title 1']
+                df['Meta Description'] = current['Meta Description 1']
+                df['H1'] = current['H1-1']
+
+                val = df[df['To'].str.contains(ROOTDOMAIN0)]
+                mainTitle = val['Title']
+                mainMeta = val['Meta Description']
+                mainH1 = val['H1']
+                df3 = pd.merge(df, df1, on='To')
+                df3 = df3[['Similarity', 'From', 'To', 'Title', 'Meta Description', 'H1']]
+                df3
+                # df3 = df3[['Similarity']]
+                # var = .30
+                # df3.loc[df3["Similarity"] < var, "New URL"] = ROOTDOMAIN
+                # df3
+                # df3.loc[df3["Similarity"] < var, "Title"] = mainTitle
+                # df3.loc[df3["Similarity"] < var, "Meta Description"] = mainMeta
+                # df3.loc[df3["Similarity"] < var, "H1"] = mainH1
+                # df3 = df3.sort_values(by='Similarity', ascending=False)
+                # df3
+
+
+            # Downloading of File
+            @st.cache
+            def convert_df(df3):
+                return df3.to_csv().encode('utf-8')
+
+
+            csv = convert_df(df3)
+            st.download_button("Download Output", csv, "file.csv", "text/csv", key='download-csv')
     if selected == "Content Estimator":
         image = Image.open('impressive.jpg')
         # Create sliders for retainer amount and average word count per page
@@ -125,7 +198,7 @@ if authentication_status:
         st.write('Total Word Count: ', total_word_count)
         st.write('Estimated Cost: ', estimated_cost)
     if selected == "Content Estimator 2":
-        #st.subheader(f"Welcome to your profile, {name}")
+        st.subheader(f"Welcome to your profile, {name}")
 
         st.write('''
             <iframe src="https://content-estimator.web.app/"
